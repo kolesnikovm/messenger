9457caf022793305f6f8a9117f92f926151b9d4d
kolesnikovm <maxxim.kolesnikov@gmail.com>
2023-12-07 17:05:11 +0300

    fix: remove odd repo method

--- src/store/interfaces.go
+++ src/store/interfaces.go
@@ -10,5 +10,4 @@ import (
 type Messages interface {
 	BatchInsert(ctx context.Context, messages []*entity.Message) error
 	GetMessageHistory(ctx context.Context, fromMessageID ulid.ULID, chatID string, messageCount uint32, direction string) ([]*entity.Message, error)
-	GetLastMessageOrderID(ctx context.Context, chatID string) (uint64, error)
 }

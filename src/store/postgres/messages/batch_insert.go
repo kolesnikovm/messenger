26a3783cbeeb9348727fb1cd1b9da9d49af0aada
kolesnikovm <maxxim.kolesnikov@gmail.com>
2023-12-10 15:39:22 +0300

    chore: add message order id handling

--- src/store/postgres/messages/batch_insert.go
+++ src/store/postgres/messages/batch_insert.go
@@ -7,32 +7,71 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/kolesnikovm/messenger/entity"
+	"github.com/rs/zerolog/log"
 )
 
-const insert = "insert into messages (id, sender_id, chat_id, text) values ($1, $2, $3, $4) on conflict (id) do update set text = $4"
+const (
+	insertMessage = "insert into messages (id, sender_id, chat_id, text, order_id) values ($1, $2, $3, $4, $5) on conflict (id) do update set text = $4"
+
+	selectMessageCounter = "select message_counter from chats where id = $1"
+	updateMessageCounter = "insert into chats (id, message_counter) values ($1, $2) on conflict (id) do update set message_counter = $2"
+)
 
 func (m *Messages) BatchInsert(ctx context.Context, messages []*entity.Message) error {
 	const op = "Messages.BatchInsert"
 
 	batches := map[*pgxpool.Pool]*pgx.Batch{}
 
+	transactions := map[*pgxpool.Pool]pgx.Tx{}
+	defer func() {
+		for _, tx := range transactions {
+			if err := tx.Rollback(ctx); err != nil && err != pgx.ErrTxClosed {
+				log.Err(err).Msgf("%s: failed to rollback transaction", op)
+			}
+		}
+	}()
+
 	for _, msg := range messages {
 		chatID := msg.GetChatID()
 		partition := m.DB.PartitionSet.Get(chatID)
 
+		tx, exists := transactions[partition]
+		if !exists {
+			var err error
+			tx, err = partition.Begin(ctx)
+			if err != nil {
+				return fmt.Errorf("%s: failed to begin transaction: %w", op, err)
+			}
+
+			transactions[partition] = tx
+		}
+
+		orderID, err := m.getMessageCounter(ctx, tx, chatID)
+		if err != nil {
+			return err
+		}
+		orderID++
+
+		err = m.updateMessageCounter(ctx, tx, chatID, orderID)
+		if err != nil {
+			return err
+		}
+
 		batch, exists := batches[partition]
 		if exists {
-			batch.Queue(insert, msg.MessageID, msg.SenderID, chatID, msg.Text)
+			batch.Queue(insertMessage, msg.MessageID, msg.SenderID, chatID, msg.Text, orderID)
 		} else {
 			b := &pgx.Batch{}
-			b.Queue(insert, msg.MessageID, msg.SenderID, chatID, msg.Text)
+			b.Queue(insertMessage, msg.MessageID, msg.SenderID, chatID, msg.Text, orderID)
 
 			batches[partition] = b
 		}
 	}
 
-	for partition, batch := range batches {
-		results := partition.SendBatch(ctx, batch)
+	for partition, tx := range transactions {
+		batch := batches[partition]
+
+		results := tx.SendBatch(ctx, batch)
 		defer results.Close()
 
 		for i := 0; i < batch.Len(); i++ {
@@ -41,6 +80,49 @@ func (m *Messages) BatchInsert(ctx context.Context, messages []*entity.Message) 
 				return fmt.Errorf("%s: %w", op, err)
 			}
 		}
+
+		results.Close()
+
+		if err := tx.Commit(ctx); err != nil {
+			return fmt.Errorf("%s: failed to commit transaction: %w", op, err)
+		}
+	}
+
+	return nil
+}
+
+func (m *Messages) getMessageCounter(ctx context.Context, tx pgx.Tx, chatID string) (uint64, error) {
+	const op = "Messages.getMessageCounter"
+
+	m.chats.mu.RLock()
+	orderID, exists := m.chats.counters[chatID]
+	m.chats.mu.RUnlock()
+
+	if !exists {
+		row := tx.QueryRow(ctx, selectMessageCounter, chatID)
+
+		if err := row.Scan(&orderID); err != nil {
+			if err == pgx.ErrNoRows {
+				return 0, nil
+			}
+			return 0, fmt.Errorf("%s: %w", op, err)
+		}
+
+	}
+
+	return orderID, nil
+}
+
+func (m *Messages) updateMessageCounter(ctx context.Context, tx pgx.Tx, chatID string, orderID uint64) error {
+	const op = "Messages.updateMessageCounter"
+
+	m.chats.mu.Lock()
+	m.chats.counters[chatID] = orderID
+	m.chats.mu.Unlock()
+
+	_, err := tx.Exec(ctx, updateMessageCounter, chatID, orderID)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
 	}
 
 	return nil
--- src/store/postgres/messages/messages.go
+++ src/store/postgres/messages/messages.go
@@ -1,6 +1,8 @@
 package messages
 
 import (
+	"sync"
+
 	"github.com/kolesnikovm/messenger/configs"
 	"github.com/kolesnikovm/messenger/store/postgres"
 )
@@ -8,11 +10,20 @@ import (
 type Messages struct {
 	DB     *postgres.DB
 	Config configs.Postgres
+	chats  *messageCounter
+}
+
+type messageCounter struct {
+	mu       sync.RWMutex
+	counters map[string]uint64
 }
 
 func New(db *postgres.DB, conf configs.Postgres) *Messages {
 	return &Messages{
 		DB:     db,
 		Config: conf,
+		chats: &messageCounter{
+			counters: map[string]uint64{},
+		},
 	}
 }

c1c9c405a98521c0983e4dcc7e95acbbc2822349
kolesnikovm <maxxim.kolesnikov@gmail.com>
2023-12-13 10:37:44 +0300

    fix: remove odd metrics

--- src/server/grpc/messenger/read_message.go
+++ src/server/grpc/messenger/read_message.go
@@ -3,15 +3,10 @@ package messenger
 import (
 	"context"
 
-	"github.com/kolesnikovm/messenger/metrics"
 	"github.com/kolesnikovm/messenger/proto"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 func (s *Handler) ReadMessage(ctx context.Context, req *proto.Message) (*proto.Message, error) {
-	metrics.ActiveStreams.With(prometheus.Labels{"type": "read"}).Inc()
-	defer metrics.ActiveStreams.With(prometheus.Labels{"type": "read"}).Dec()
-
 	userID := ctx.Value(StringContextKey("userID")).(uint64)
 
 	m, err := convertPbToEntity(req)
